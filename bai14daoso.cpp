a8935f47f5f23c2f9b670beb98ba16337ec14320
DHTri <145561569+DHTri@users.noreply.github.com>
2023-11-15 12:07:26 +0700

    Create bai14daoso.cpp

---
+++ bai14daoso.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,sum;
+    cin >> n;
+    int sum2 = 0;
+    while(n > 0){
+        sum = n % 10;
+        sum2 = sum2 * 10 + sum ;
+        n = n / 10;
+    }
+    cout << sum2;
+}
